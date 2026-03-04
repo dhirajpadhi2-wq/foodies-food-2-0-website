c200bb9adf5d1902eb7cff868ea08f6541a42324
dhirajpadhi2-wq <dhirajpadhi2@gmail.com>
2026-03-04 15:08:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function sendWhatsApp(){
+let name = document.getElementById("name").value;
+let order = document.getElementById("order").value;
 
+let phone = "919938467223";
+
+let url = "https://wa.me/" + phone + 
+"?text=Hello Foodies Food 2.0,%0AName: " + name + 
+"%0AOrder: " + order;
+
+window.open(url, '_blank');
+}
